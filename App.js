bafa0a261d47b84269f04b63e1b2897c7817919c
Shreya Gupta <shreya11gupta.13@gmail.com>
2022-02-06 23:17:20 -0600

    .

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
+
+export default function App() {
+  return(
+    <LoginScreen/>
+  );
+}
---
+++ app/config/Colors.js
@@ -0,0 +1,9 @@
+export default {
+    c1: "#b22222",
+    c2: "#dc143c",
+    c3: "#ff4500",
+    c4: "#ffa500",
+    c5: "#ffd700",    
+    white: "#fff",
+    black: "#000"
+}
